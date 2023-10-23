de8dbbece6d2458570b1db4b52427a2f35d32801
Technoknight17 <112963679+Technoknight17@users.noreply.github.com>
2023-10-22 20:17:54 -0400

    All Pages Formatted

---
+++ js/javascript.js
@@ -0,0 +1,13 @@
+function displayText1() {
+  document.getElementById("hiddenDefault1").style.display = "block";
+}
+
+function displayText2() {
+  var text = document.getElementById("textField");
+  text.style.display = "block";
+}
+
+function displayText3() {
+  var text = document.getElementById("textField");
+  text.style.display = "block";
+}
